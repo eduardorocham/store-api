710a42acd1b7b2c047c5539de47bee236db0784e
@eduardo <jose.eduardo@inbec.edu.br>
2024-02-05 17:54:21 -0300

    getCustomers method created

--- src/controllers/CustomersController.js
+++ src/controllers/CustomersController.js
@@ -1,11 +1,15 @@
-const Controller = require('./Controller.js');
-const CustomersService = require('../services/CustomersService.js');
+const Controller = require("./Controller.js");
+const CustomersService = require("../services/CustomersService.js");
 
 const customerService = new CustomersService();
 
 class CustumersController extends Controller {
   constructor() {
-    super(customerService, 'customer');
+    super(customerService, "customer");
+  }
+
+  async getCustomers(req, res, next) {
+    const { name, email, start_created_date, end_created_date } = req.query;
   }
 }
 
